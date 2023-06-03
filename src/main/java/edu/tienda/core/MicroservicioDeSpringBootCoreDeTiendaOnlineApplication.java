b8eb6ba67d336c628d4d9b9a1989d0d7548cd7be
yunierab <yunierab88@gmail.com>
2023-06-03 15:19:04 -0300

    Primer commit

---
+++ src/main/java/edu/tienda/core/MicroservicioDeSpringBootCoreDeTiendaOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tienda.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDeSpringBootCoreDeTiendaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDeSpringBootCoreDeTiendaOnlineApplication.class, args);
+	}
+
+}
